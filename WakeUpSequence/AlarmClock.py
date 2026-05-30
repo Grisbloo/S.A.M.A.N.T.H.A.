518f4648cf3213036d138945a49eb94aa8d349b0
Bloo <nightshadowsilencer@gmail.com>
2026-05-30 12:23:09 -0500

    Create AlarmClock.py

---
+++ WakeUpSequence/AlarmClock.py
@@ -0,0 +1,5 @@
+# AlarmClock python file to be written
+# TODO: API fetch function
+# TODO: Text-to-speech Function to be the alarm of the daily brief
+# TODO: Speech-to Text Function to be the way to turn off the alarm of the daily brief
+# TODO: RF transmitter to garage door function
